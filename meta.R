4ed7350bbb656c6925529dca614ce9c9a8e18189
Vanessa Poon <vanessa.poon16@gmail.com>
2021-03-29 11:41:30 +0100

    barplots with text wrapping

--- meta.R
+++ meta.R
@@ -1,10 +1,6 @@
 setwd("~/OneDrive - University of Birmingham/Year 3/Research Project/data") 
-# setwd("data")
-getwd()
 
 library(tidyverse)
-# install.packages("cowplot")
-# install.packages("hrbrthemes")
 library(cowplot)
 library(hrbrthemes)
 
@@ -14,9 +10,7 @@ names(results_all) <- plyr::mapvalues(x = names(results_all), from = "GSE146028_
 
 datasets <- read.delim("Datasets.txt")
 
-# sub <- datasets$ID [- c(2,4,6,7)]
 sub <- datasets$ID
-
 
 #creates new lists of just log2FC and padj from results_all
 lfc <- list()
@@ -42,13 +36,16 @@ Symbol <- AnnotationDbi::mapIds(org.Hs.eg.db, keys = substr(genes, 1, 15), keyty
 
 # selected gene targets
 # classic m1/ m2 markers
-targets  <- c("TNF", "IL6", "NOS2", "IL12B", "IL23A", "CXCL9", "CXCL10", "CXCL11", "IL10", "ARG1", "CCL17", "CCL22", "TGFB1") 
+targets  <- c("TNF", "IL6", "NOS2", "IL12B", "IL23A", "CXCL9", "CXCL10", "CXCL11", "IL10", "ARG1", "CCL17", "CCL22", "TGFB1", "CD14", "CD16", "NOS2", "CD163", "CD80", "MRC1", "ITGAM", "IRF5", "IFNG", "STAT1", "IRF4", "STAT3") 
 # bmp antagonists 
-# targets <- c("CHRD", "CHRDL1", "DAND5", "FST", "FSTL1", "FSTL3", "GREM1", "GREM2", "NBL1", "NOG", "SOST", "SOSTDC1", "TWSG1")
-# bmps 
-# targets <- c("BMP2", "BMP4", "BMP7", "BMPR1A", "BMPR1B", "BMPR2", "BMPER", "ENG", "MIF", "CD44", "CD74", "MIF", "KDR")
+# targets <- c("CER1","CHRD", "CHRDL1", "DAND5", "FST", "FSTL1", "FSTL3", "GREM1", "GREM2", "NBL1", "NOG", "SOST", "SOSTDC1", "TWSG1")
+# bmp pathway
+# targets <- c("BAMBI", "BMP2", "BMP4", "BMP6", "BMP7", "BMPR1A", "BMPR1B", "BMPR2", "MAP3K","MAPK1", "SMAD1", "SMAD4","SMAD5", "SMAD6", "SMAD7" , "SMAD9")
+# TGFB pathway
+# targets <- c("APC", "CDH1", "CREBBP", "EP300", "MAP2K1", "MAP3K7", "MAPK3", "SKIL", "SMAD2", "SMAD3", "SMAD4", "SMAD7", "TAB1", "TGFB1", "TGFB2", "TGFB3", "TGFBR1", "TGFBR2", "ZFYVE9")
+# activin receptor pathway
+# targets  <- c("ACVR1", "ACVR1B", "ACVR1C", "ACVR2A", "ACVR2B", "ACVRL1", "BMP10", "BMPR2", "CER1", "CFC1", "CFC1B", "SMAD2", "SMAD3", "SMAD7", "DAND5", "FGF10", "GDF2", "GDF6", "GDF7", "NODAL")
 
-# targets <- c("GREM1")
 
 #heatmap for selected genes
 #column number needs to match number of datsets for pivot_longer
@@ -69,28 +66,26 @@ lfc_long$signif <- case_when(
 
 max <- max(abs(lfc_long$value), na.rm = T)
 p1 <- lfc_long %>% 
-  ggplot(aes(x = factor(name, level = c("GSE100382_LPS", "GSE135753_LPS", "GSE163165_LPS", "GSE98368_IFNg", "GSE146028_IFNg.LPS", "GSE132732_IL4", "GSE146028_IL4", "GSE123603_IL4")), y = gene_id, fill = value))+
+  ggplot(aes(x = factor(name, level = c("GSE146028_IFNg.LPS", "GSE98368_IFNg", "GSE100382_LPS", "GSE163165_LPS", "GSE135753_LPS", "GSE146028_IL4", "GSE132732_IL4", "GSE123603_IL4")), y = gene_id, fill = value))+
   geom_tile()+
   geom_text(aes(label = signif))+
-  labs(title = "Expression of classical markers", x ="", y = "", 
+  labs(title = "", x ="", y = "", 
        fill = expression(paste("log" [2], "-fc")))+
   scale_fill_distiller(type = "div", palette = "RdBu", limits = c(-max, max)) +
   scale_y_discrete(limits = rev(levels(lfc_long$gene_id)))+
   theme_minimal()+
   theme(line = element_line(colour = "black", size = 0.5),
-        text = element_text(family = "serif"),
-        plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
+        #text = element_text(family = "serif"),
+        # plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
         panel.background = element_blank(), panel.grid = element_blank(),
         legend.title = element_text(size = 10, face = "bold"),
         legend.key = element_blank(),
         axis.line = element_line(size = 0.5, colour = "black"),
         axis.ticks = element_line(size = 0.5, colour = "black"),
-        axis.title = element_text(size = 10, face = "bold", hjust = 0.5),
-        axis.text.x = element_text(size=10, face = "bold", angle = 90, hjust = 1),
-        axis.text.y = element_text(size=10, face = "bold"))
+        axis.title = element_text(size = 10, hjust = 0.5),
+        axis.text.x = element_text(size=10, angle = 90, hjust = 1),
+        axis.text.y = element_text(size=10))
 p1
-
-#install.packages("corrplot")
 
 #check for correlation of datasets
 lfc_mat <- as.matrix(lfc_select)
@@ -143,8 +138,8 @@ invnormcomb_LPS <- invnorm(rawpval,nrep=c(2,3,3), BHth = 0.05)  #save separately
 sum(invnormcomb_LPS$adjpval < 0.01, na.rm = T) #save separately
 
 DEresults_LPS <- data.frame(DE_LPS,
-                        "DE.fishercomb"=ifelse(fishcomb_LPS$adjpval<=0.01,1,0),
-                        "DE.invnorm"=ifelse(invnormcomb_LPS$adjpval<=0.01,1,0)) #rename DEresults object
+                            "DE.fishercomb"=ifelse(fishcomb_LPS$adjpval<=0.01,1,0),
+                            "DE.invnorm"=ifelse(invnormcomb_LPS$adjpval<=0.01,1,0)) #rename DEresults object
 head(DEresults_LPS)
 
 #remove contradictory differential expression
@@ -238,27 +233,27 @@ IFNgandLPS <- results_all$`GSE146028_IFNg+LPS`
 IFNg <-  IFNg %>% dplyr::select(log2FoldChange, padj) 
 
 #subsetting up and downregulated genes
-IFNg_up<- filter(IFNg, log2FoldChange > 0, !is.na(padj))
-IFNg_down <-  filter(IFNg, log2FoldChange < 0, !is.na(padj))
+IFNg_up<- dplyr::filter(IFNg, log2FoldChange > 0, !is.na(padj))
+IFNg_down <-  dplyr::filter(IFNg, log2FoldChange < 0, !is.na(padj))
 
 IFNgandLPS <- IFNgandLPS %>% dplyr::select(log2FoldChange, padj)
 
-IFNgandLPS_up <-  filter(IFNgandLPS, log2FoldChange > 0, !is.na(padj))
-IFNgandLPS_down <-  filter(IFNgandLPS, log2FoldChange < 0, !is.na(padj))
+IFNgandLPS_up <-  dplyr::filter(IFNgandLPS, log2FoldChange > 0, !is.na(padj))
+IFNgandLPS_down <-  dplyr::filter(IFNgandLPS, log2FoldChange < 0, !is.na(padj))
 
 library(AnnotationDbi)
 library(org.Hs.eg.db)
 library(GenomicFeatures)
 
-gtf <- "gencode.gtf"
-txdb <- makeTxDbFromGFF(gtf)
-saveDb(txdb,"txdb.filename")
+# gtf <- "gencode.gtf"
+# txdb <- makeTxDbFromGFF(gtf)
+# saveDb(txdb,"txdb.filename")
 
-txdf <- AnnotationDbi::select(txdb, keys(txdb, "GENEID"), "TXNAME", "GENEID")
-tab <- table(txdf$GENEID)
-txdf$ntx <- tab[match(txdf$GENEID, names(tab))]
-k <- keys(txdb, keytype = "TXNAME")
-tx2gene <- AnnotationDbi::select(txdb, k, "GENEID", "TXNAME")
+# txdf <- AnnotationDbi::select(txdb, keys(txdb, "GENEID"), "TXNAME", "GENEID")
+# tab <- table(txdf$GENEID)
+# txdf$ntx <- tab[match(txdf$GENEID, names(tab))]
+# k <- keys(txdb, keytype = "TXNAME")
+# tx2gene <- AnnotationDbi::select(txdb, k, "GENEID", "TXNAME")
 
 #adding gene symbols to gene lists
 IFNg_up <-as.data.frame(IFNg_up) %>%
@@ -309,21 +304,24 @@ IFNgandLPS_down_gene_list <- IFNgandLPS_down$genes [IFNgandLPS_down$padj < 0.01]
 IFNgandLPS_down_gene_list <- substr(IFNgandLPS_down_gene_list, 1, 15) 
 IFNgandLPS_down_gene_list
 
-
 library(clusterProfiler)
-
-enr_GO <- enrichGO(IFNgandLPS_down_gene_list, org.Hs.eg.db, keyType = "ENSEMBL", ont = "BP")
+enr_GO <- enrichGO(DE_genes_up_IL4, org.Hs.eg.db, keyType = "ENSEMBL", ont = "BP")
 enr_GO <- setReadable(enr_GO, org.Hs.eg.db, keyType = "ENSEMBL") #converting to gene symbols
 head(enr_GO)
 
 #barplot to show enriched pathways
 library(enrichplot)
-barplot(enr_GO, showCategory=10) +
-  labs(title = "Downregulated pathways in IFNg+LPS stimulated macrophages", y = "Counts")
+barplot(enr_GO, showCategory=10, label_format=21) +
+  labs(title = "IL4", x = "Counts") + 
+  expand_limits(x = c(0, 150)) +
+  theme(plot.title = element_text(size = 12, hjust = 0.5),
+        axis.text.y = element_text(size=10)) 
+
+
 
 #adding gene symbols 
 LPS_invnorm <- as.data.frame(invnormcomb_LPS$adjpval) %>%
-  mutate(genes = rownames(invnormcomb_LPS$adjpval)) %>%
+  mutate(genes = rownames(DE_genes_all_LPS)) %>%
   mutate(Symbol = mapIds(
     org.Hs.eg.db,
     keys = substr(genes, 1, 15),
@@ -331,7 +329,7 @@ LPS_invnorm <- as.data.frame(invnormcomb_LPS$adjpval) %>%
     column = "SYMBOL"))
 
 LPS_fish <- as.data.frame(fishcomb_LPS$adjpval) %>%
-  mutate(genes = rownames(fishcomb_LPS$adjpval)) %>%
+  mutate(genes = rownames(DE_genes_all_LPS)) %>%
   mutate(Symbol = mapIds(
     org.Hs.eg.db,
     keys = substr(genes, 1, 15),
@@ -339,7 +337,7 @@ LPS_fish <- as.data.frame(fishcomb_LPS$adjpval) %>%
     column = "SYMBOL"))
 
 IL4_invnorm <- as.data.frame(invnormcomb_IL4$adjpval) %>%
-  mutate(genes = rownames(invnormcomb_IL4$adjpval)) %>%
+  mutate(genes = rownames(DE_genes_all_IL4)) %>%
   mutate(Symbol = mapIds(
     org.Hs.eg.db,
     keys = substr(genes, 1, 15),
@@ -395,12 +393,164 @@ minpadj_df <- as.data.frame(apply(allpadj_df, 1, min, na.rm=TRUE))
 
 names(minpadj_df)[names(minpadj_df) == "apply(allpadj_df, 1, min, na.rm = TRUE)"] <- "minimum_padj"
 
-minpadj_df <- minpadj_df %>% filter(minimum_padj > 0) %>% arrange(minimum_padj) %>% top_n(-1000)
+minpadj_df <- minpadj_df %>% filter(minimum_padj > 0) %>% arrange(minimum_padj) %>% top_n(-2000) 
 
-#creating a matrix of log2foldchagnes of top 1000 regulated genes for creating the overview heatmap
+#creating matrix of log2foldchagnes of top 1000 regulated genes for creating overview heatmap
 matrix_heatmap_overview <- lfc_df [rownames(lfc_df) %in% rownames(minpadj_df), ] %>% as.matrix()
+library(pheatmap)
+library(ComplexHeatmap)
+library(dendsort)
+library(dendextend)
+library(circlize)
+library(RColorBrewer)
+library(viridis)
+library(gridtext)
 
-pheatmap::pheatmap(matrix_heatmap_overview,
-                   show_rownames = FALSE,
-                   scale = "row")
+mat_cluster_rows <- hclust(dist(matrix_heatmap_overview), method = "ward.D")
+# mat_cluster_rows <- color_branches(mat_cluster_rows, k = 4)
+plot(mat_cluster_rows, main = "Unsorted Dendrogram", xlab = "", sub = "")
 
+sort_hclust <- function(...) as.hclust(dendsort(as.dendrogram(...), isReverse=TRUE, type="min"))
+
+plot(mat_cluster_rows, main = "Sorted Dendrogram", xlab = "", sub = "")
+mat_cluster_cols <- sort_hclust(hclust(dist(t(matrix_heatmap_overview))), method = "ward.D")
+
+row_cluster <- cutree(mat_cluster_rows, k = 4)
+row_cluster <- plyr::mapvalues(row_cluster, from = c(1,2,3,4), to = c(2,4,1,3))
+row_cluster <- factor(row_cluster)
+
+# Data frame with row cluster annotations.
+mat_row <- data.frame(cluster = as.factor(row_cluster))
+rownames(mat_row) <- names(row_cluster)
+
+# List with colors for each annotation.
+mat_colors <- list(cluster = brewer.pal(length(unique(row_cluster)), "YlGn"))
+names(mat_colors$cluster) <- unique(row_cluster)
+
+# pheatmap::pheatmap(matrix_heatmap_overview,
+# show_rownames = FALSE,
+# cluster_cols = mat_cluster_cols,
+# cluster_rows = mat_cluster_rows,
+#  drop_levels = TRUE,
+# annotation_row = mat_row,
+#  annotation_color = mat_colors,
+#   scale = "row")
+
+enr_GO_clust <- list()
+enr_GO_clust_df <- list()
+
+for (i in 1:length(unique(row_cluster))){
+  geneList <- substr(names(row_cluster) [row_cluster == i], 1, 15)
+  enr_GO_clust[[i]] <- enrichGO(geneList, ont = "BP", OrgDb = org.Hs.eg.db, keyType = "ENSEMBL",
+                                pvalueCutoff = 0.05)
+  enr_GO_clust_df[[i]] <- head(enr_GO_clust[[i]], n = "Inf")
+}
+
+enr_KEGG_clust <- list()
+enr_KEGG_clust_df <- list()
+
+for (i in 1:length(unique(row_cluster))){
+  geneList <- substr(names(row_cluster) [row_cluster == i], 1, 15)
+  geneList <- mapIds(org.Hs.eg.db, geneList, keytype = "ENSEMBL", column = "ENTREZID",
+                     pvalueCutoff = 0.05)
+  enr_KEGG_clust[[i]] <- enrichKEGG(geneList, organism = "hsa")
+  enr_KEGG_clust_df[[i]] <- head(enr_KEGG_clust[[i]], n = "Inf")
+}
+
+text_list = list(
+  text1 = "complement activation \nresponse to IFNg & LPS \nmacrophage activation",
+  text2 = "vesicle organisation \nRas and Rab signal transduction \nregulation of cell size",
+  text3 = "autophagy",
+  text4 = "metabolic regulation \nleukocyte differentiation \nECM organisation \nERK1 and ERK2 signaling"
+)
+
+ha = HeatmapAnnotation(bar = anno_empty(row_cluster, border = FALSE,
+                                        width = max_text_width(unlist(text_list)) + unit(4, "mm")),
+                       which = "row")
+
+Heatmap(matrix_heatmap_overview,
+        cluster_rows = mat_cluster_rows,
+        cluster_columns = mat_cluster_cols,
+        heatmap_legend_param = list(title = expression(paste("log" [2], "-fc"))),
+        right_annotation = ha,
+        row_split = 4,
+        show_row_names = FALSE)
+for(i in 1:4) {
+  decorate_annotation("bar", slice = i, {
+    grid.rect(x = 0, width = unit(2, "mm"), gp = gpar(fill = i, col = NA), just = "left")
+    grid.text(paste(text_list[[i]], collapse = "\n"), x = unit(4, "mm"), just = "left")
+  })
+}
+
+#heatmap for selected genes with ComplexHeatmap package####
+
+
+# selected gene targets
+# classic m1/ m2 markers
+targets  <- c("TNF", "IL6", "NOS2", "IL12B", "IL23A", "CXCL9", "CXCL10", "CXCL11", "IL10", "ARG1", "CCL17", "CCL22", "TGFB1", "CD14", "CD16", "NOS2", "CD163", "CD80", "MRC1", "ITGAM", "IRF5", "IFNG", "STAT1", "IRF4", "STAT3") 
+# bmp antagonists 
+# targets <- c("CER1","CHRD", "CHRDL1", "DAND5", "FST", "FSTL1", "FSTL3", "GREM1", "GREM2", "NBL1", "NOG", "SOST", "SOSTDC1", "TWSG1")
+# bmp pathway
+# targets <- c("BAMBI", "BMP2", "BMP4", "BMP6", "BMP7", "BMPR1A", "BMPR1B", "BMPR2", "MAP3K","MAPK1", "SMAD1", "SMAD4","SMAD5", "SMAD6", "SMAD7" , "SMAD9")
+# TGFB pathway
+# targets <- c("APC", "CDH1", "CREBBP", "EP300", "MAP2K1", "MAP3K7", "MAPK3", "SKIL", "SMAD2", "SMAD3", "SMAD4", "SMAD7", "TAB1", "TGFB1", "TGFB2", "TGFB3", "TGFBR1", "TGFBR2", "ZFYVE9")
+# activin receptor pathway
+# targets  <- c("ACVR1", "ACVR1B", "ACVR1C", "ACVR2A", "ACVR2B", "ACVRL1", "BMP10", "BMPR2", "CER1", "CFC1", "CFC1B", "SMAD2", "SMAD3", "SMAD7", "DAND5", "FGF10", "GDF2", "GDF6", "GDF7", "NODAL")
+# targets <- c("GREM1")
+
+lfc_select <- lfc_df [Symbol %in% targets, ] %>% as.matrix()
+rownames(lfc_select) <- Symbol [Symbol %in% targets]
+padj_select <- padj_df [Symbol %in% targets, ] %>% as.matrix()
+rownames(padj_select) <- Symbol [Symbol %in% targets]
+
+
+padj_asterisk <- matrix(case_when(
+  padj_select < 0.001 ~ "***",
+  padj_select < 0.01 ~ "**",
+  padj_select < 0.05 ~ "*"), ncol = ncol(padj_select))
+
+meta <- data.frame(LPS = case_when(apply(DEresults_LPS[,4:6], 1, sum) == 3 ~ apply(cbind(fishcomb_LPS$adjpval, invnormcomb_LPS$adjpval), 1, max),
+                                   apply(DEresults_LPS[,4:6], 1, sum) != 3 ~ 1),
+                   IL4 = case_when(apply(DEresults_IL4[,4:6], 1, sum) == 3 ~ apply(cbind(fishcomb_IL4$adjpval, invnormcomb_IL4$adjpval), 1, max),
+                                   apply(DEresults_IL4[,4:6], 1, sum) != 3 ~ 1)
+)
+meta <- meta [Symbol %in% targets, ]
+rownames(meta) <- Symbol [Symbol %in% targets]
+
+ha = rowAnnotation("LPS" = anno_text(case_when(
+  meta$LPS < 0.001 ~ "***",
+  meta$LPS < 0.01 ~ "**",
+  meta$LPS < 0.05 ~ "*",
+  meta$LPS >= 0.05 ~ "",
+  is.na(meta$LPS) ~ "" ),
+  width = unit(6.5, "mm")),
+  "IL4" = anno_text(case_when(
+    meta$IL4 < 0.001 ~ "***",
+    meta$IL4 < 0.01 ~ "**",
+    meta$IL4 < 0.05 ~ "*",
+    meta$IL4 >=0.05 ~ "",
+    is.na(meta$IL4) ~ ""),
+    width = unit(6.5, "mm")),
+  annotation_name_gp = gpar(fontsize = 11),
+  annotation_name_side = "top",
+  annotation_name_rot = 0,
+  show_annotation_name = TRUE
+)
+ha_col = HeatmapAnnotation(Col = anno_empty(which = "column", colnames(lfc_select), border = F, height = unit(5, "mm")))
+
+p3 <- Heatmap(lfc_select,
+              cell_fun = function(j, i, x, y, width, heigth, fill) {
+                if(!is.na(padj_asterisk[i, j]))
+                  grid.text(padj_asterisk[i,j], x, y, gp = gpar(fontsize = 10))
+              },
+              heatmap_legend_param = list(title = expression(paste("log" [2], "-fc"))),
+              cluster_rows = FALSE, cluster_columns = FALSE,
+              show_row_dend = FALSE, row_names_side = "left",
+              right_annotation = ha,
+              top_annotation = ha_col
+)
+
+p3@right_annotation@anno_list[["LPS"]]@name_param[["show"]] <- TRUE
+p3@right_annotation@anno_list[["IL4"]]@name_param[["show"]] <- TRUE
+
+p3
